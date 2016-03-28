b8a8de0fe586e6e4673b33b41d65fbfea0ba4f46
kotohana5706 <kotohana5706@edcan.kr>
2016-03-28 10:24:46 +0900

    initial commit

---
+++ Shoot20409/Assets/script/backScroll.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backScroll : MonoBehaviour {
+
+
+    public float scrollSpeed = 0.7f;
+    Material myMaterial;
+
+	// Use this for initialization
+	void Start () {
+        myMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newOffsetY = myMaterial.mainTextureOffset.y + scrollSpeed * Time.deltaTime;
+        Vector2 newOffset = new Vector2(0, newOffsetY);
+        myMaterial.mainTextureOffset = newOffset;
+
+	}
+}
